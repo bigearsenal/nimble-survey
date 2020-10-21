c6b556770a74d52b311cc1fa3c2f13ac9c64d7ff
Chung Tran <bigearsenal@gmail.com>
2020-10-21 17:01:34 +0700

    layout login screen

---
+++ nimble/Extensions/Numeric+Extensions.swift
@@ -0,0 +1,25 @@
+//
+//  CGFloat+Extensions.swift
+//  nimble
+//
+//  Created by Chung Tran on 10/21/20.
+//
+
+import Foundation
+
+extension CGFloat {
+    // iPhone X as design template
+    static let heightRatio: CGFloat = UIScreen.main.bounds.height / (UIApplication.shared.statusBarOrientation.isPortrait ? 812 : 375)
+    static let widthRatio: CGFloat = UIScreen.main.bounds.width / (UIApplication.shared.statusBarOrientation.isPortrait ? 375 : 812)
+}
+
+extension BinaryInteger {
+    
+    var adaptiveWidth: CGFloat {
+        (CGFloat(self) * CGFloat.widthRatio).rounded(.down)
+    }
+
+    var adaptiveHeight: CGFloat {
+        (CGFloat(self) * CGFloat.heightRatio).rounded(.down)
+    }
+}
--- nimble/LoginViewController.swift
+++ nimble/LoginViewController.swift
@@ -10,10 +10,10 @@ import Foundation
 class LoginViewController: BaseViewController {
     override var padding: UIEdgeInsets {.init(top: 24, left: 24, bottom: 24, right: 24)}
     
-    lazy var nimbleLogo = UIImageView(width: 167, height: 40, imageNamed: "nimble_logo")
-    lazy var emailField = UITextField(height: 56, backgroundColor: UIColor.white.withAlphaComponent(0.18), cornerRadius: 10, placeholder: "Email", autocorrectionType: .no, textContentType: .emailAddress, leftView: UIView(width: 12), leftViewMode: .always, rightView: UIView(width: 12), rightViewMode: .always)
-    lazy var passwordField = UITextField(height: 56, backgroundColor: UIColor.white.withAlphaComponent(0.18), cornerRadius: 10, placeholder: "Password", autocorrectionType: .no, spellCheckingType: .no, isSecureTextEntry: true, horizontalPadding: 12, rightView: forgotButton.padding(UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)), rightViewMode: .always)
-    lazy var loginButton = UIButton(height: 56, backgroundColor: .white, cornerRadius: 10, label: "Login", labelFont: .systemFont(ofSize: 17, weight: .semibold), textColor: .black)
+    lazy var nimbleLogo = UIImageView(width: 167.adaptiveHeight, height: 40.adaptiveHeight, imageNamed: "nimble_logo")
+    lazy var emailField = UITextField(height: 56.adaptiveHeight, backgroundColor: UIColor.white.withAlphaComponent(0.18), cornerRadius: 10, placeholder: "Email", autocorrectionType: .no, autocapitalizationType: UITextAutocapitalizationType.none, textContentType: .emailAddress, leftView: UIView(width: 12), leftViewMode: .always, rightView: UIView(width: 12), rightViewMode: .always)
+    lazy var passwordField = UITextField(height: 56.adaptiveHeight, backgroundColor: UIColor.white.withAlphaComponent(0.18), cornerRadius: 10, placeholder: "Password", autocorrectionType: .no, spellCheckingType: .no, isSecureTextEntry: true, horizontalPadding: 12, rightView: forgotButton.padding(UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)), rightViewMode: .always)
+    lazy var loginButton = UIButton(height: 56.adaptiveHeight, backgroundColor: .white, cornerRadius: 10, label: "Login", labelFont: .systemFont(ofSize: 17, weight: .semibold), textColor: .black)
     lazy var forgotButton = UIButton(label: "Forgot?", textColor: UIColor.white.withAlphaComponent(0.5))
     
     override func setUp() {
@@ -26,21 +26,47 @@ class LoginViewController: BaseViewController {
         
         // add anythings to stackView
         stackView.spacing = 20
-        stackView.addArrangedSubview(UIView(height: 80)) // setCustomSpacing is only avalable in iOS 11, so I create 'spacer' view for spacing instead
+        stackView.addArrangedSubview(UIView(height: 80.adaptiveHeight)) // setCustomSpacing is only avalable in iOS 11, so I create 'spacer' view for spacing instead
         stackView.addArrangedSubview(nimbleLogo.centeredHorizontallyView)
-        stackView.addArrangedSubview(UIView(height: 80))
+        stackView.addArrangedSubview(UIView(height: 80.adaptiveHeight))
         stackView.addArrangedSubview(emailField)
         stackView.addArrangedSubview(passwordField)
         stackView.addArrangedSubview(loginButton)
         
         view.layoutIfNeeded()
-//        nimbleLogo.isHidden = true
         
+        animate()
     }
     
     func animate() {
-        let tempLogo = UIImageView(width: 201, height: 48, imageNamed: "nimble_logo")
-        view.addSubview(tempLogo)
+        // prepare
+        let blurEffect = UIBlurEffect(style: .dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = self.view.bounds
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.view.insertSubview(blurEffectView, at: 1)
         
+        blurEffectView.alpha = 0
+        emailField.alpha = 0
+        passwordField.alpha = 0
+        loginButton.alpha = 0
+        
+        let originalTransform = nimbleLogo.transform
+        let originalCenter = nimbleLogo.center
+        
+        let scaledTransform = originalTransform.scaledBy(x: 1.2, y: 1.2)
+        nimbleLogo.transform = scaledTransform
+        nimbleLogo.center = stackView.convert(stackView.center, from:scrollView.contentView)
+        
+        UIView.animate(withDuration: 0.3, delay: 0.5) {
+            self.nimbleLogo.transform = originalTransform
+            self.nimbleLogo.center = originalCenter
+            blurEffectView.alpha = 1
+            self.emailField.alpha = 1
+            self.passwordField.alpha = 1
+            self.loginButton.alpha = 1
+        } completion: { (_) in
+            
+        }
     }
 }
