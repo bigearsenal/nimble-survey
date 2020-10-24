11e74f35d4557a4e9514b11c29234ddd4cc3ae88
Chung Tran <bigearsenal@gmail.com>
2020-10-24 14:31:28 +0700

    add clientId, secret

--- nimble/Services/NimbleSurveySDK.swift
+++ nimble/Services/NimbleSurveySDK.swift
@@ -10,14 +10,20 @@ import Foundation
 struct NimbleSurveySDK {
     // MARK: - Properties
     let endpoint: String
+    let clientId: String
+    let clientSecret: String
     
     // MARK: - Singleton
     static let shared = NimbleSurveySDK()
     private init() {
         #if DEBUG
         endpoint = "https://nimble-survey-web-staging.herokuapp.com/"
+        clientId = ""
+        clientSecret = ""
         #else
         endpoint = "https://survey-api.nimblehq.co/"
+        clientId = "ofzl-2h5ympKa0WqqTzqlVJUiRsxmXQmt5tkgrlWnOE"
+        clientSecret = "lMQb900L-mTeU-FVTCwyhjsfBwRCxwwbCitPob96cuU"
         #endif
     }
 }
