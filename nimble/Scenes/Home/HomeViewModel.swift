80b13a5a70ef0d06abcd2862c4dedad3169666f2
Chung Tran <bigearsenal@gmail.com>
2020-10-25 10:08:50 +0700

    add HomeViewModel

--- nimble/Scenes/Home/HomeViewModel.swift
+++ nimble/Scenes/Home/HomeViewModel.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import RxSwift
+
+class HomeViewModel: BaseViewModel<[ResponseSurvey]> {
+    let apiSDK: APISDK
+    
+    init(sdk: APISDK = NimbleSurveySDK.shared) {
+        apiSDK = sdk
+        super.init()
+    }
+    
+    override func request() -> Single<[ResponseSurvey]> {
+        // TODO: Pagination
+        apiSDK.getSurveysList(pageNumber: 1, pageSize: 2)
+    }
+}
