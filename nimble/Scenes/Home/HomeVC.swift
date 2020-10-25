847da069e208d3e065390896c1205b0020732b8b
Chung Tran <bigearsenal@gmail.com>
2020-10-25 14:04:14 +0700

    refactor HomeVC

--- nimble/Scenes/Home/HomeVC.swift
+++ nimble/Scenes/Home/HomeVC.swift
@@ -15,38 +15,14 @@ class HomeVC: BaseViewController {
     
     // MARK: - Subviews
     lazy var bgImageView = UIImageView(forAutoLayout: ())
+    lazy var fullDatelabel = UILabel(textSize: 13, textColor: .white)
+    lazy var shortDateLabel = UILabel(textSize: 34, weight: .semibold, textColor: .white)
     lazy var avatarImageView = UIImageView(width: 36, height: 36, cornerRadius: 18)
     
-    lazy var topLoadingStackView: UIStackView = {
-        let stackView = UIStackView(axis: .vertical, spacing: 10, alignment: .leading, distribution: .fill)
-        stackView.addArrangedSubview(createLoadingView(width: 117).setTag(loadingTag))
-        stackView.addArrangedSubview(createLoadingView(width: 100).setTag(loadingTag))
-        return stackView
-    }()
+    lazy var topLoadingStackView = createTopLoadingView()
+    lazy var bottomLoadingStackView = createBottomLoadingView()
     
-    lazy var bottomLoadingStackView: UIStackView = {
-        let stackView = UIStackView(axis: .vertical, spacing: 0, alignment: .leading, distribution: .fill)
-        stackView.addArrangedSubview(createLoadingView(width: 40).setTag(loadingTag))
-        stackView.addArrangedSubview(UIView(height: 16))
-        stackView.addArrangedSubview(createLoadingView(width: 254).setTag(loadingTag))
-        stackView.addArrangedSubview(UIView(height: 8))
-        stackView.addArrangedSubview(createLoadingView(width: 127).setTag(loadingTag))
-        stackView.addArrangedSubview(UIView(height: 16))
-        stackView.addArrangedSubview(createLoadingView(width: 318).setTag(loadingTag))
-        stackView.addArrangedSubview(UIView(height: 8))
-        stackView.addArrangedSubview(createLoadingView(width: 212).setTag(loadingTag))
-        return stackView
-    }()
-    
-    lazy var errorView: UIStackView = {
-        let stackView = UIStackView(axis: .vertical, spacing: 10, alignment: .center, distribution: .fill)
-        stackView.addArrangedSubview(errorLabel)
-        stackView.addArrangedSubview(
-            UIButton(backgroundColor: .white, cornerRadius: 10, label: "retry", textColor: .black, contentInsets: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10))
-                .onTap(self, action: #selector(reload))
-        )
-        return stackView
-    }()
+    lazy var errorView = createErrorView()
     lazy var errorLabel = UILabel(textSize: 17, weight: .medium, textColor: .white, numberOfLines: 0, textAlignment: .center)
     
     override func setUp() {
@@ -55,27 +31,11 @@ class HomeVC: BaseViewController {
         view.backgroundColor = UIColor(red: 21/255, green: 21/255, blue: 26/255, alpha: 1)
         view.addSubview(bgImageView)
         
-        // header stackView
-        let headerStackView: UIStackView = {
-            let stackView = UIStackView(axis: .horizontal, spacing: 10, alignment: .center, distribution: .fill)
-//            stackView.addArrangedSubview(topLoadingStackView)
-            stackView.addArrangedSubview(UIView(forAutoLayout: ()))
-            stackView.addArrangedSubview(avatarImageView)
-            return stackView
-        }()
+        // configure header
+        configureHeader()
         
-        view.addSubview(headerStackView)
-        headerStackView.autoPinEdge(toSuperviewSafeArea: .top, withInset: 16)
-        headerStackView.autoPinEdge(toSuperviewSafeArea: .leading, withInset: 20)
-        headerStackView.autoPinEdge(toSuperviewSafeArea: .trailing, withInset: 20)
-        
-        view.addSubview(topLoadingStackView)
-        topLoadingStackView.autoPinToTopLeftCornerOfSuperviewSafeArea(xInset: 20, yInset: 16)
-        topLoadingStackView.arrangedSubviews.filter {$0.tag == loadingTag}.forEach {$0.showLoading()}
-        
-        view.addSubview(bottomLoadingStackView)
-        bottomLoadingStackView.autoPinToBottomLeftCornerOfSuperviewSafeArea(xInset: 20, yInset: 33)
-        bottomLoadingStackView.arrangedSubviews.filter {$0.tag == loadingTag}.forEach {$0.showLoading()}
+        // add loading views
+        addLoadingViews()
         
         // load data
         reload()
@@ -89,6 +49,8 @@ class HomeVC: BaseViewController {
                 self?.setUpWithLoadingState(loadingState)
             })
             .disposed(by: disposeBag)
+        
+        viewModel.
     }
     
     func setUpWithLoadingState(_ state: HomeViewModel.LoadingState) {
@@ -97,11 +59,15 @@ class HomeVC: BaseViewController {
         case .loading:
             bgImageView.isHidden = true
             topLoadingStackView.isHidden = false
+            fullDatelabel.isHidden = true
+            shortDateLabel.isHidden = true
             avatarImageView.showLoading()
             bottomLoadingStackView.isHidden = false
         case .loaded:
             bgImageView.isHidden = false
             topLoadingStackView.isHidden = true
+            fullDatelabel.isHidden = false
+            shortDateLabel.isHidden = false
             avatarImageView.hideLoading()
             bottomLoadingStackView.isHidden = true
         case .error(let error):
@@ -110,6 +76,9 @@ class HomeVC: BaseViewController {
             bgImageView.isHidden = true
             topLoadingStackView.isHidden = true
             bottomLoadingStackView.isHidden = true
+            fullDatelabel.isHidden = true
+            shortDateLabel.isHidden = true
+            avatarImageView.hideLoading()
         }
     }
     
@@ -119,7 +88,67 @@ class HomeVC: BaseViewController {
     }
     
     // MARK: - Helpers
+    private func configureHeader() {
+        // header stackView
+        let headerStackView: UIStackView = {
+            let stackView = UIStackView(axis: .horizontal, spacing: 10, alignment: .center, distribution: .fill)
+            let labelsStackView = UIStackView(axis: .vertical, spacing: 4, alignment: .leading, distribution: .fill)
+            labelsStackView.addArrangedSubview(fullDatelabel)
+            labelsStackView.addArrangedSubview(shortDateLabel)
+            stackView.addArrangedSubview(labelsStackView)
+            stackView.addArrangedSubview(UIView(forAutoLayout: ()))
+            stackView.addArrangedSubview(avatarImageView)
+            return stackView
+        }()
+        
+        view.addSubview(headerStackView)
+        headerStackView.autoPinEdge(toSuperviewSafeArea: .top, withInset: 16)
+        headerStackView.autoPinEdge(toSuperviewSafeArea: .leading, withInset: 20)
+        headerStackView.autoPinEdge(toSuperviewSafeArea: .trailing, withInset: 20)
+    }
+    
     private func createLoadingView(width: CGFloat) -> UIView {
         UIView(width: width, height: 16, cornerRadius: 8)
+    }
+    
+    private func createTopLoadingView() -> UIStackView {
+        let stackView = UIStackView(axis: .vertical, spacing: 10, alignment: .leading, distribution: .fill)
+        stackView.addArrangedSubview(createLoadingView(width: 117).setTag(loadingTag))
+        stackView.addArrangedSubview(createLoadingView(width: 100).setTag(loadingTag))
+        return stackView
+    }
+    
+    private func createBottomLoadingView() -> UIStackView {
+        let stackView = UIStackView(axis: .vertical, spacing: 0, alignment: .leading, distribution: .fill)
+        stackView.addArrangedSubview(createLoadingView(width: 40).setTag(loadingTag))
+        stackView.addArrangedSubview(UIView(height: 16))
+        stackView.addArrangedSubview(createLoadingView(width: 254).setTag(loadingTag))
+        stackView.addArrangedSubview(UIView(height: 8))
+        stackView.addArrangedSubview(createLoadingView(width: 127).setTag(loadingTag))
+        stackView.addArrangedSubview(UIView(height: 16))
+        stackView.addArrangedSubview(createLoadingView(width: 318).setTag(loadingTag))
+        stackView.addArrangedSubview(UIView(height: 8))
+        stackView.addArrangedSubview(createLoadingView(width: 212).setTag(loadingTag))
+        return stackView
+    }
+    
+    private func createErrorView() -> UIStackView {
+        let stackView = UIStackView(axis: .vertical, spacing: 10, alignment: .center, distribution: .fill)
+        stackView.addArrangedSubview(errorLabel)
+        stackView.addArrangedSubview(
+            UIButton(backgroundColor: .white, cornerRadius: 10, label: "retry", textColor: .black, contentInsets: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10))
+                .onTap(self, action: #selector(reload))
+        )
+        return stackView
+    }
+    
+    private func addLoadingViews() {
+        view.addSubview(topLoadingStackView)
+        topLoadingStackView.autoPinToTopLeftCornerOfSuperviewSafeArea(xInset: 20, yInset: 16)
+        topLoadingStackView.arrangedSubviews.filter {$0.tag == loadingTag}.forEach {$0.showLoading()}
+        
+        view.addSubview(bottomLoadingStackView)
+        bottomLoadingStackView.autoPinToBottomLeftCornerOfSuperviewSafeArea(xInset: 20, yInset: 33)
+        bottomLoadingStackView.arrangedSubviews.filter {$0.tag == loadingTag}.forEach {$0.showLoading()}
     }
 }
